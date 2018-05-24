2601e9797e5541be304770ad247cb27759477314
Unknown <putri.liza@mhs.unsyiah.ac.id>
2018-05-24 22:50:03 +0700

    penambahan tombol logout

--- Logout.php
+++ Logout.php
@@ -1,9 +1,7 @@
 <?php
-include('connection.php');
 
-session_start();
 if(isset($_GET['logout'])){
-session_destroy();
-unset($_SESSION['id_pelanggan']);
-header("location : SignIn.php");
+		session_destroy();
+		unset($_SESSION['Nama']);
+	}
 ?>
