909dff051df10f3dd03a24ced8b7a05820585050
medi darmawan <medidarmawan47@gmail.com>
2018-06-05 21:56:16 +0700

    fungsionalitas delete,tambah dan tampil menu admin

---
+++ actioninsertdata.php
@@ -0,0 +1,33 @@
+<?php
+
+$server = "localhost";
+$user   = "root";
+$password   = "";
+$dbname     = "rpl";
+
+
+
+
+$conn = mysqli_connect($server, $user, $password, $dbname);
+
+if (!$conn){
+    die("gagal connect : " . mysqli_connect_error());
+    
+}
+
+
+$menu =$_POST['menu'];
+$kategori =$_POST['kategori'];
+$harga =$_POST['harga'];
+
+$sql = "INSERT INTO nama_menu (Nama_Menu, Kategori, Harga) VALUES ('$menu','$kategori','$harga')";
+
+if (mysqli_query($conn, $sql))
+{  
+  header("Location: /ADMIN/tambahmenu.php");
+}
+else {
+    echo "Error :" . $sql . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
