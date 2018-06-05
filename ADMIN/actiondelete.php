f5f5911ae87d9d69688ada2a0118518612b3e27b
medi darmawan <38047822+medidarmawan@users.noreply.github.com>
2018-06-05 22:42:20 +0700

    UPDATE actiondelete

--- ADMIN/actiondelete.php
+++ ADMIN/actiondelete.php
@@ -5,5 +5,5 @@ $id_namamenu = $_GET['id_namamenu'];
 $sql = 'DELETE FROM nama_menu WHERE id_namamenu=:id_namamenu';
 $statement = $connection->prepare($sql);
 if ($statement->execute([':id_namamenu' => $id_namamenu])) {
-  header("Location: /ADMIN/deletemenu.php"); 
+  header("Location: ../ADMIN/deletemenu.php"); 
 }
